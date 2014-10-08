1dc041418f164f4e070f99c3a4ea6ae656593d2c
Luka Ruklić <ruklic.luka@gmail.com>
2014-10-08 23:41:58 +0200

    Dodano iscrtavanje heksagona na ploču.

--- NeuroshimaHex/src/main/java/hr/nhex/graphic/NeuroshimaCanvas.java
+++ NeuroshimaHex/src/main/java/hr/nhex/graphic/NeuroshimaCanvas.java
@@ -4,18 +4,29 @@ import hr.nhex.generic.Pair;
 import hr.nhex.graphic.hexagon.Hexagon;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ * Class that represents canvas that contains NHex playing board.
+ * 
+ * @author Luka Rukliæ
+ * @author Marin Bužanèiæ
+ *
+ */
+
 public class NeuroshimaCanvas extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 
 	private JFrame mainWindow;
-	private double hexSize = 5;
+	
+	private int hexSize;
 	
 	private List<Hexagon> hexagonList = new ArrayList<>();
 	
@@ -30,24 +41,35 @@ public class NeuroshimaCanvas extends JPanel {
 				
 		for (int m = -2; m <= 2; m++) {
 			for (int n = -2; n <= 2; n++) {
-				if (Math.abs(m + n) > 2) {
-					double x = (windowWidth/2) + ((-3/2)*hexSize)*n;
-					double y = (windowHeight/2) + ((Math.sqrt(2)/2)*hexSize*n) + (Math.sqrt(3)*hexSize*m);
-					hexagonList.add(new Hexagon((int)x,(int)y,3));
+				if (Math.abs(m + n) <= 2) {
+					double x = (windowWidth/2) + (Math.sqrt(3)*hexSize)*m+((Math.sqrt(3)/2)*hexSize)*n;
+					double y = (windowHeight/2) + ((-3/2)*hexSize)*n;
+					
+					int hexSizeX = windowHeight / 6;
+					int hexSizeY = (int) (windowWidth / (7*(Math.sqrt(3)/2)));
+					
+					if (hexSizeX < hexSizeY) {
+						hexSize = hexSizeX;
+					} else {
+						hexSize = hexSizeY;
+					}
+					
+					hexagonList.add(new Hexagon((int)x,(int)y,hexSize));
 				}
 			}
 		}
 		System.out.println("hex board init success");
-		for (Hexagon h : hexagonList) {
-			h.paint(this.getGraphics());
-		}
 		
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
-		// TODO Auto-generated method stub
-		super.paintComponents(g);
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		super.paintComponent(g2);
+		for (Hexagon h : hexagonList) {
+			h.drawHex(g2);
+		}
 	}
 	
 }
--- NeuroshimaHex/src/main/java/hr/nhex/graphic/hexagon/Hexagon.java
+++ NeuroshimaHex/src/main/java/hr/nhex/graphic/hexagon/Hexagon.java
@@ -1,6 +1,11 @@
 package hr.nhex.graphic.hexagon;
 
+import hr.nhex.graphic.hexgame;
+
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
 
 import javax.swing.JComponent;
 
@@ -8,6 +13,7 @@ import javax.swing.JComponent;
  * Class that represents graphical interpretation of hexagon on the NHex board.
  * 
  * @author Luka Rukliæ
+ * @author Marin Bužanèiæ
  *
  */
 
@@ -26,7 +32,7 @@ public class Hexagon extends JComponent{
 	/**
 	 * Distance between center of the hexagon and any of its six tops.
 	 */
-	private int centerTopDistance;
+	private int hexSize;
 	
 	/**
 	 * Constructor for class hexagon.
@@ -35,16 +41,43 @@ public class Hexagon extends JComponent{
 	 * @param y
 	 * @param centerTopDistance
 	 */
-	public Hexagon(int x, int y, int centerTopDistance) {
+	public Hexagon(int x, int y, int hexSize) {
 		super();
 		this.x = x;
 		this.y = y;
-		this.centerTopDistance = centerTopDistance;
+		this.hexSize = hexSize;
 	}
+	
+	public Polygon createHex(int xC, int yC) {
+		
+		int x0 = xC; 
+		int x1 = (int)(xC + (hexSize*Math.sqrt(3)/2));
+		int x2 = x1;
+		int x3 = x0;
+		int x4 = (int)(xC - (hexSize*Math.sqrt(3)/2));;
+		int x5 = x4;
+		
+		int y0 = yC - hexSize;		
+		int y1 = (int)(yC - (hexSize*0.5));
+		int y2 = (int)(yC + (hexSize*0.5));
+		int y3 = yC + hexSize;
+		int y4 = y2;
+		int y5 = y1;
+		
+		int[] arrayX = new int[] {x0,x1,x2,x3,x4,x5};  //this is for the top left vertex being at x,y. Which means that some of the hex is cutoff.
+		int[] arrayY = new int[] {y0,y1,y2,y3,y4,y5};	
 
-	@Override
-	public void paint(Graphics g) {
-		g.drawLine(x, y, x+2, y+2);
+		return new Polygon(arrayX,arrayY,6);
+	}
+ 	
+	public void drawHex(Graphics2D g2) {
+
+		Polygon poly = createHex(x,y);
+		g2.setColor(Color.RED);
+		//g2.fillPolygon(hexmech.hex(x,y));
+		g2.fillPolygon(poly);
+		g2.setColor(Color.BLACK);
+		g2.drawPolygon(poly);
 	}
 	
 }

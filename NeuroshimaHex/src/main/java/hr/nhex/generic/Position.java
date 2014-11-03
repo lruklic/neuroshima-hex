e35ed2cc42187d9b4a78c6fcdf5466187e8176ef
Luka Ruklić <ruklic.luka@gmail.com>
2014-11-04 00:29:04 +0100

    Dodan razred Position.

---
+++ NeuroshimaHex/src/main/java/hr/nhex/generic/Position.java
@@ -0,0 +1,24 @@
+package hr.nhex.generic;
+
+public class Position {
+
+	private double x;
+	private double y;
+
+	public Position(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+
+
+
+}
